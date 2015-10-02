be730349c1dc7b267b62e3cd395091f350d2e345
Aloïs KYROU <projectawesome1100@gmail.com>
2015-10-02 20:46:17 +0200

    Game Over and restart added

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    private bool gameOver = false;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+	    if(GameObject.FindGameObjectWithTag("Player")==null)
+        {
+            Debug.Log("i pass here");
+            gameOver = true;
+        }
+        if (gameOver)
+        {
+            if (Input.anyKeyDown)
+            {
+                Application.LoadLevel(Application.loadedLevel);
+            }
+        }
+	}
+
+    void OnGUI()
+    {
+        if (gameOver == true)
+        {
+            GUILayout.Label("Game Over ! Press any key to reset");
+        }
+    }
+}
