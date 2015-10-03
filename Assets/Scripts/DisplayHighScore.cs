e3ccdaf081e95b7878dd59e67e09c7823ed9551d
Aloïs KYROU <projectawesome1100@gmail.com>
2015-10-03 20:58:13 +0200

    Previous score and highscore added on title screen

---
+++ Assets/Scripts/DisplayHighScore.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DisplayHighScore : MonoBehaviour {
+
+    private string label;
+
+	// Use this for initialization
+	void Start ()
+    {
+        TextMesh textMesh= GetComponent<TextMesh>();
+        label = textMesh.text;
+        textMesh.text = label + PlayerPrefs.GetInt("Highscore");
+	}
+	
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,9 +3,33 @@ using System.Collections;
 
 public class GameManager : MonoBehaviour {
 
+    private static GameManager _instance; //means this exists doesn't matter how mush instance of the game only one 
+
     public float pointsPerUnitTravelled = 1.0f;
     public float gameSpeed = 10.0f;
-    public static GameManager Instance; //means this exists doesn't matter how mush instance of the game only one 
+    public string titleScreenName = "TitleScreen";
+
+    public static GameManager Instance
+    {
+        get
+        {
+            if(_instance != null)
+            {
+                return _instance;
+            }
+            else
+            {
+                GameObject gameManager =  new GameObject("GameManager");
+                _instance =  gameManager.AddComponent<GameManager>();
+                return _instance;
+
+            }
+            
+        }
+    }
+
+    [HideInInspector]
+    public int previousScore = 0; //we don't want to be visible in inspector but need to access the value from an other script so public
 
     private float score = 0.0f;
     private bool gameOver = false;
@@ -14,40 +38,67 @@ public class GameManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        Instance = this; //then we can access GameManager.Instance from every other function
+        if (_instance != this)
+        {
+            if (_instance == null)
+            {
+                _instance = this; //then we can access GameManager.Instance from every other function
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
         loadhighScore();
+        DontDestroyOnLoad(gameObject); //by default everything is destroyed on launch of scene
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
-	    if(GameObject.FindGameObjectWithTag("Player")==null)
+        if (Application.loadedLevelName != titleScreenName) //if not in title screen do game logic
         {
-            gameOver = true;
-        }
-        if (gameOver)
-        {
-            if (!hasSaved)
+            if (GameObject.FindGameObjectWithTag("Player") == null)
             {
-                SaveHighScore();
-                hasSaved = true;
+                gameOver = true;
             }
-            if (Input.anyKeyDown)
+            if (gameOver)
             {
-                Application.LoadLevel(Application.loadedLevel);
+                if (!hasSaved)
+                {
+                    SaveHighScore();
+                    previousScore = (int)score;
+                    hasSaved = true;
+                }
+                if (Input.anyKeyDown)
+                {
+                    Application.LoadLevel(titleScreenName);
+                }
+            }
+
+            if (!gameOver)
+            {
+                score += pointsPerUnitTravelled * gameSpeed * Time.deltaTime;
+                if (score > highScore)
+                {
+                    highScore = score;
+                }
             }
         }
 
-        if (!gameOver)
-        {          
-            score += pointsPerUnitTravelled * gameSpeed * Time.deltaTime;
-            if (score > highScore)
-            {
-                highScore = score;
-            }
+        else
+        {
+            //reset stuff for next game
+            ResetGame();
         }
     }
 
+    void ResetGame()
+    {
+        score = 0.0f;
+        gameOver = false;
+        hasSaved = false;
+    }
 
     void SaveHighScore()
     {
@@ -62,13 +113,16 @@ public class GameManager : MonoBehaviour {
 
     void OnGUI()
     {
-        int currentHighScore = (int)highScore;
-        int currentScore = (int)score;
-        GUILayout.Label("Score : " + currentScore.ToString());
-        GUILayout.Label("Highscore : " + currentHighScore.ToString());
-        if (gameOver == true)
+        if (Application.loadedLevelName != titleScreenName)
         {
-            GUILayout.Label("Game Over ! Press any key to reset");
+            int currentHighScore = (int)highScore;
+            int currentScore = (int)score;
+            GUILayout.Label("Score : " + currentScore.ToString());
+            GUILayout.Label("Highscore : " + currentHighScore.ToString());
+            if (gameOver == true)
+            {
+                GUILayout.Label("Game Over ! Press any key to quit");
+            }
         }
     }
 }
